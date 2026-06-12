29969edf4baacfd58949dfff0b8c0c3e7c2dcf73
Kajal Agarwal <kajalagarwal0701@Kajals-MacBook-Air.local>
2026-06-12 16:03:19 +0530

    please happen

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def home():
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/static/style.css">
 </head>
 <body>
